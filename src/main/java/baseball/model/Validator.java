cfaa4bc14d141d81624b8b19d0f140610e200fb2
imsongj <imsongj@gmail.com>
2022-11-29 02:23:08 +0900

    feat(baseball) : 중복되는 숫자 예외 처리 기능 구현

--- src/main/java/baseball/model/Validator.java
+++ src/main/java/baseball/model/Validator.java
@@ -1,5 +1,9 @@
 package baseball.model;
 
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
 import java.util.regex.Pattern;
 
 public class Validator {
@@ -7,7 +11,11 @@ public class Validator {
     public static void validateNumberInput(String input) {
         validateNumeric(input);
         validateSize(input);
-        validateRange(input);
+        List<Integer> numbers = input.chars()
+                .map(Character::getNumericValue)
+                .collect(ArrayList::new, List::add, List::addAll);
+        validateRange(numbers);
+        validateDuplicate(numbers);
     }
 
     public static void validateRestartInput(String input) {
@@ -28,14 +36,19 @@ public class Validator {
         }
     }
 
-    public static void validateRange(String input) {
-        int number = Integer.parseInt(input);
-        for (int index = 0; index < BaseballStatistic.NUMBERS_SIZE.getValue(); index++) {
-            if (number % 10 < BaseballStatistic.LOWER_BOUND.getValue()
-                    || number % 10 > BaseballStatistic.UPPER_BOUND.getValue()) {
+    public static void validateRange(List<Integer> numbers) {
+        for (int number : numbers) {
+            if (number < BaseballStatistic.LOWER_BOUND.getValue()
+                    || number > BaseballStatistic.UPPER_BOUND.getValue()) {
                 throw new IllegalArgumentException();
             }
-            number /= 10;
+        }
+    }
+
+    public static void validateDuplicate(List<Integer> numbers) {
+        Set<Integer> numbersWithoutDuplicates = new HashSet<>(numbers);
+        if (numbersWithoutDuplicates.size() != BaseballStatistic.NUMBERS_SIZE.getValue()) {
+            throw new IllegalArgumentException();
         }
     }
 }
--- src/test/java/baseball/model/ValidatorTest.java
+++ src/test/java/baseball/model/ValidatorTest.java
@@ -2,9 +2,13 @@ package baseball.model;
 
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
+import java.util.List;
+import java.util.stream.Stream;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.MethodSource;
 
 public class ValidatorTest {
     @ParameterizedTest
@@ -32,13 +36,32 @@ public class ValidatorTest {
     }
 
     @ParameterizedTest
-    @CsvSource({
-            "012",
-            "230"
-    })
+    @MethodSource("provideParametersForRange")
     @DisplayName("입력 숫자 범위를 벗어나면 예외를 발생시킨다.")
-    void throwExceptionForInvalidRange(String input) {
-        assertThatThrownBy(() -> Validator.validateRange(input))
+    void throwExceptionForInvalidRange(List<Integer> numbers) {
+        assertThatThrownBy(() -> Validator.validateRange(numbers))
                 .isInstanceOf(IllegalArgumentException.class);
+    }
+    private static Stream<Arguments> provideParametersForRange() {
+        return Stream.of(
+                Arguments.of(List.of(1, 2, 0)),
+                Arguments.of(List.of(10, 2, 3)),
+                Arguments.of(List.of(-1, 2, 0))
+        );
+    }
+
+    @ParameterizedTest
+    @MethodSource("provideParametersForDuplicate")
+    @DisplayName("중복되는 숫자가 존재하면 예외를 발생시킨다.")
+    void throwExceptionForDuplicate(List<Integer> numbers) {
+        assertThatThrownBy(() -> Validator.validateDuplicate(numbers))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+    private static Stream<Arguments> provideParametersForDuplicate() {
+        return Stream.of(
+                Arguments.of(List.of(1, 2, 1)),
+                Arguments.of(List.of(10, 3, 3)),
+                Arguments.of(List.of(1, 1, 1))
+        );
     }
 }
